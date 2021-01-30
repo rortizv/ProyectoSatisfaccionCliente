beb7fe2d0dd04a804a76c8849494d9f9af3825f7
rafae <rafae@DESKTOP-GJQEAG6>
2021-01-30 17:09:55 -0500

    1

---
+++ src/proyectosatisfaccioncliente/ProyectoSatisfaccionCliente.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosatisfaccioncliente;
+import ventanas.principal;
+
+/**
+ *
+ * @author rafae
+ */
+public class ProyectoSatisfaccionCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        principal Principal = new principal();
+        Principal.setVisible(true);
+        
+    }
+    
+}
